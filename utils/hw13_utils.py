42392924226e6b0d93fabd35c3d4b558a8e71cd9
Farid.Bagirov <farid.bagirov@jetbrains.com>
2022-02-23 17:10:37 +0300

    add hw13 utils

---
+++ utils/hw13_utils.py
@@ -0,0 +1,32 @@
+from .utils import *
+from pytorch_utils import *
+
+
+def visualize_q3_data():
+    data_dir = get_data_dir(2)
+    train_data, test_data = load_pickled_data(join(data_dir, 'celeb.pkl'))
+    name = 'CelebA'
+
+    idxs = np.random.choice(len(train_data), replace=False, size=(100,))
+    images = train_data[idxs].astype(np.float32) / 3.0 * 255.0
+    show_samples(images, title=f'{name} Samples')
+
+def get_q3_data():
+    data_dir = get_data_dir(2)
+    train_data, test_data = load_pickled_data(join(data_dir, 'celeb.pkl'))
+    return train_data, test_data
+
+
+def q3_save_results(fn, part):
+    data_dir = get_data_dir(2)
+    train_data, test_data = load_pickled_data(join(data_dir, 'celeb.pkl'))
+
+    train_losses, test_losses, samples, interpolations = fn(train_data, test_data)
+    samples = samples.astype('float')
+    interpolations = interpolations.astype('float')
+
+    print(f'Final Test Loss: {test_losses[-1]:.4f}')
+    save_training_plot(train_losses, test_losses, f'Q3 Dataset Train Plot',
+                       f'results/q3_{part}_train_plot.png')
+    show_samples(samples * 255.0, f'results/q3_{part}_samples.png')
+    show_samples(interpolations * 255.0, f'results/q3_{part}_interpolations.png', nrow=6, title='Interpolations')
